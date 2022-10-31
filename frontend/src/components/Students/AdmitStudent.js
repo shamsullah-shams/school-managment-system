1c03cbc5572ef664cfbf8e8311fb8c0e949dc55e
shamsullah-shams <shamsullah07073@gmail.com>
2022-10-31 22:08:41 +0430

    one tiny bug

--- frontend/src/components/Students/AdmitStudent.js
+++ frontend/src/components/Students/AdmitStudent.js
@@ -14,7 +14,6 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const AdmitStudent = props => {
 
-    const [isFirstPart, setIsFirstPart] = useState(true);
     // @@ Fetch Classes, Parents, Sections from Redux
     const classes = useSelector(state => state.loadTeachers.Classes);
     const parents = useSelector(state => state.loadTeachers.Parents);
@@ -88,146 +87,6 @@ const AdmitStudent = props => {
 
 
 
-    // Change Form to First 
-    const onFormChangeHandlerToFirstPart = event => {
-        setIsFirstPart(true);
-    }
-
-    // Change Form to First 
-    const onFormChangeHandlerToSecondPart = event => {
-        setIsFirstPart(false);
-    }
-
-
-    const SecondPart = () => (
-        <React.Fragment>
-            <Row>
-                <SelectElement
-                    label="Class"
-                    options={classes}
-                    name="className"
-                    value={student.className}
-                    onChange={onChangeHandler}
-                />
-                <SelectElement
-                    label="Section"
-                    options={sections}
-                    name="section"
-                    value={student.section}
-                    onChange={onChangeHandler}
-                />
-                <SelectElement
-                    label="Parent"
-                    options={parents}
-                    name="parent"
-                    value={student.parent}
-                    onChange={onChangeHandler}
-                />
-                <InputElement
-                    label="Admition No"
-                    placeholder="Admission"
-                    name="addmissionNo"
-                    value={student.addmissionNo}
-                    onChange={onChangeHandler}
-                />
-            </Row>
-            <Row>
-                <Buttons title="Submit" onClick={onSubmitHandler} />
-            </Row>
-        </React.Fragment>
-    );
-
-    const FirstPart = () => (
-        <React.Fragment>
-
-            <Row>
-                <InputElement
-                    label="Full Name"
-                    placeholder="Full Name"
-                    name="fullName"
-                    value={student.fullName}
-                    onChange={onChangeHandler}
-                />
-                <InputElement
-                    label="Address"
-                    placeholder="Address"
-                    name="address"
-                    value={student.address}
-                    onChange={onChangeHandler}
-                />
-            </Row>
-            <Row>
-                <InputElement
-                    label="Email Address"
-                    placeholder="Email Address"
-                    name="email"
-                    value={student.email}
-                    onChange={onChangeHandler}
-                />
-                <SelectElement
-                    label="Gender"
-                    options={["Male", "Female"]}
-                    name="gender"
-                    value={student.gender}
-                    onChange={onChangeHandler}
-                />
-                <InputElement
-                    label="Phone"
-                    placeholder="Phone"
-                    type="tele"
-                    name="phone"
-                    value={student.phone}
-                    onChange={onChangeHandler}
-                />
-            </Row>
-            <Row>
-                <InputElement
-                    label="Date Of Birth"
-                    placeholder="Date Of Birth"
-                    type="date"
-                    name="dateOfBirth"
-                    value={student.dateOfBirth}
-                    onChange={onChangeHandler}
-                />
-                <SelectElement
-                    label="Nationality"
-                    options={["Afghan", "Indian", "pakistani", "Americon"]}
-                    name="nationality"
-                    value={student.nationality}
-                    onChange={onChangeHandler}
-                />
-                <SelectElement
-                    label="State"
-                    options={["Amreica", "Asia", "Urop", "North America"]}
-                    name="state"
-                    value={student.state}
-                    onChange={onChangeHandler}
-                />
-            </Row>
-            <Row>
-                <SelectElement
-                    label="Blood Group"
-                    options={["A+", "A-", "B+", "B-", "O+", "O-", "AB+", "AB-"]}
-                    name="bloodGroup"
-                    value={student.bloodGroup}
-                    onChange={onChangeHandler}
-                />
-                <InputElement
-                    label="Choose Image"
-                    type="file"
-                    name="image"
-                    onChange={onChangeHandler}
-                />
-            </Row>
-            <Row>
-                <Buttons title="Next" onClick={onFormChangeHandlerToSecondPart} />
-            </Row>
-        </React.Fragment>
-    );
-
-
-
-
 
     return (
         <React.Fragment>
@@ -236,28 +95,120 @@ const AdmitStudent = props => {
                 autoClose={5000}
                 toastStyle={{ background: "rgb(43, 42, 42)", color: "#fff" }}
             />
-            <div className="Group__Div">
-                <div
-                    className="Circle Active_Circle"
-                    onClick={onFormChangeHandlerToFirstPart}
-                >
-                    1
-                </div>
-                <svg width="500" height="500"  >
-                    <line x1="0" y1="12" x2="500" y2="12" stroke={isFirstPart ? "black" : "blue"} />
-                </svg>
-                <div
-                    className={isFirstPart ? "Circle" : "Circle Active_Circle"}
-                    onClick={onFormChangeHandlerToSecondPart}
-                >
-                    2
-                </div>
-            </div>
 
             <Form onSubmit={onSubmitHandler}>
-                {
-                    isFirstPart ? <FirstPart /> : <SecondPart />
-                }
+                <Row>
+                    <InputElement
+                        label="Full Name"
+                        placeholder="Full Name"
+                        name="fullName"
+                        value={student.fullName}
+                        onChange={onChangeHandler}
+                    />
+                    <InputElement
+                        label="Address"
+                        placeholder="Address"
+                        name="address"
+                        value={student.address}
+                        onChange={onChangeHandler}
+                    />
+                </Row>
+                <Row>
+                    <InputElement
+                        label="Email Address"
+                        placeholder="Email Address"
+                        name="email"
+                        value={student.email}
+                        onChange={onChangeHandler}
+                    />
+                    <SelectElement
+                        label="Gender"
+                        options={["Male", "Female"]}
+                        name="gender"
+                        value={student.gender}
+                        onChange={onChangeHandler}
+                    />
+                    <InputElement
+                        label="Phone"
+                        placeholder="Phone"
+                        type="tele"
+                        name="phone"
+                        value={student.phone}
+                        onChange={onChangeHandler}
+                    />
+                </Row>
+                <Row>
+                    <InputElement
+                        label="Date Of Birth"
+                        placeholder="Date Of Birth"
+                        type="date"
+                        name="dateOfBirth"
+                        value={student.dateOfBirth}
+                        onChange={onChangeHandler}
+                    />
+                    <SelectElement
+                        label="Nationality"
+                        options={["Afghan", "Indian", "pakistani", "Americon"]}
+                        name="nationality"
+                        value={student.nationality}
+                        onChange={onChangeHandler}
+                    />
+                    <SelectElement
+                        label="State"
+                        options={["Amreica", "Asia", "Urop", "North America"]}
+                        name="state"
+                        value={student.state}
+                        onChange={onChangeHandler}
+                    />
+                </Row>
+                <Row>
+                    <SelectElement
+                        label="Blood Group"
+                        options={["A+", "A-", "B+", "B-", "O+", "O-", "AB+", "AB-"]}
+                        name="bloodGroup"
+                        value={student.bloodGroup}
+                        onChange={onChangeHandler}
+                    />
+                    <InputElement
+                        label="Choose Image"
+                        type="file"
+                        name="image"
+                        onChange={onChangeHandler}
+                    />
+                </Row>
+                <Row>
+                    <SelectElement
+                        label="Class"
+                        options={classes}
+                        name="className"
+                        value={student.className}
+                        onChange={onChangeHandler}
+                    />
+                    <SelectElement
+                        label="Section"
+                        options={sections}
+                        name="section"
+                        value={student.section}
+                        onChange={onChangeHandler}
+                    />
+                    <SelectElement
+                        label="Parent"
+                        options={parents}
+                        name="parent"
+                        value={student.parent}
+                        onChange={onChangeHandler}
+                    />
+                    <InputElement
+                        label="Admition No"
+                        placeholder="Admission"
+                        name="addmissionNo"
+                        value={student.addmissionNo}
+                        onChange={onChangeHandler}
+                    />
+                </Row>
+                <Row>
+                    <Buttons title="Submit" onClick={onSubmitHandler} />
+                </Row>
             </Form>
 
         </React.Fragment>
